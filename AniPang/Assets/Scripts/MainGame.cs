0992903832844d23a78da6c75bf27b79621e0c39
hulkSTD <hulk1257@gmail.com>
2018-07-31 13:04:18 +0900

    Some Error Main Function

--- AniPang/Assets/Scripts/MainGame.cs
+++ AniPang/Assets/Scripts/MainGame.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MainGame : MonoBehaviour {
 
     private GameObject[,] unit;
+    private GameObject[,] copy;
     public string[] character;
 
 	// Use this for initialization
@@ -32,12 +33,206 @@ public class MainGame : MonoBehaviour {
                 unit[i,j] = Spawn(spawn, new Vector3((float)(-5.5 + i), (float)(-9.5 + j), -1.0f));
             }
         }
+
+        InvokeRepeating("CheckField", 0f, 0.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+    void CheckField()
+    {
+        copy = unit;
+            
+        Debug.Log("2");
+        for (int j = 0; j < 20; j++)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                if (unit[i, j] == null)
+                {
+                    Debug.Log("스폰중");
+
+                    GameObject spawn = Resources.Load("Prefabs/" + character[Random.Range(0, 9)]) as GameObject;
+
+                    unit[i, j] = Spawn(spawn, new Vector3((float)(-5.5 + i), (float)(-9.5 + j), -1.0f));
+                }
+            }
+        }
+
+        for (int i = 0; i < 12; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                bool flag = Check(i, j);
+                if(flag)
+                {
+                    DownUnit();
+                }
+            }
+        }
+    }
+
+    void DownUnit()
+    {
+        for (int j = 1; j < 20; j++)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                if (unit[i, j - 1] == null)
+                {
+                    Debug.Log("!");
+                    unit[i, j - 1] = unit[i, j];
+                    unit[i, j] = null;
+                }
+            }
+        }
+
+        for (int j = 0; j < 20; j++)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                if (unit[i, j] == null)
+                {
+                    Debug.Log("스폰중");
+
+                    GameObject spawn = Resources.Load("Prefabs/" + character[Random.Range(0, 9)]) as GameObject;
+
+                    unit[i, j] = Spawn(spawn, new Vector3((float)(-5.5 + i), (float)(-9.5 + j), -1.0f));
+                }
+            }
+        }
+    }
+
+    public bool CheckPang(GameObject a, GameObject b)
+    {
+        copy = unit;
+
+        Vector2Int q, w;
+
+        q = new Vector2Int();
+        w = new Vector2Int();
+
+        for (int i = 0; i < 12; i++) 
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                if(copy[i,j] == a)
+                {
+                    q.x = i;
+                    q.y = j;
+                }
+                if(copy[i, j] == b)
+                {
+                    w.x = i;
+                    w.y = j;
+                }
+            }
+        }
+
+        GameObject tmp = copy[q.x, q.y];
+        copy[q.x, q.y] = copy[w.x, w.y];
+        copy[w.x, w.y] = tmp;
+
+        bool Check1 = Check(w.x, w.y);
+        bool Check2 = Check(q.x, q.y);
+
+        if(!Check1 && !Check2)
+        {
+            Debug.Log("1");
+            return false;
+        }
+
+        DownUnit();
+        
+        return true;
+    }
+
+    private bool Check(int x, int y)
+    {
+        bool flag = false;
+        List<Vector2Int> CheckList = new List<Vector2Int>();
+
+        CheckList.Add(new Vector2Int(x, y));
+        if (!(x - 2 < 0 || x > 11 || y < 0 || y > 19 ))
+        {
+            if (copy[x, y].name == copy[x - 1, y].name && copy[x, y].name == copy[x - 2, y].name)
+            {
+                CheckList.Add(new Vector2Int(x - 1, y));
+                CheckList.Add(new Vector2Int(x - 2, y));
+                flag = true;
+            }
+        }
+
+        if (!(x - 1 < 0 || x + 1 > 11 || y < 0 || y > 19 ))
+        {
+            if (copy[x, y].name == copy[x - 1, y].name && copy[x, y].name == copy[x + 1, y].name)
+            {
+                CheckList.Add(new Vector2Int(x - 1, y));
+                CheckList.Add(new Vector2Int(x + 1, y));
+                flag = true;
+            }
+        }
+
+
+        if (!(x < 0 || x + 2 > 11 || y < 0 || y > 19 ))
+        {
+            if (copy[x, y].name == copy[x + 2, y].name && copy[x, y].name == copy[x + 1, y].name)
+            {
+                CheckList.Add(new Vector2Int(x + 2, y));
+                CheckList.Add(new Vector2Int(x + 1, y));
+                flag = true;
+            }
+        }
+
+        if (!(x < 0 || x > 11 || y < 0 || y + 2 > 19 ))
+        {
+            if (copy[x, y].name == copy[x, y + 1].name && copy[x, y].name == copy[x, y + 2].name)
+            {
+                CheckList.Add(new Vector2Int(x, y + 2));
+                CheckList.Add(new Vector2Int(x, y + 1));
+                flag = true;
+            }
+        }
+
+        if (!(x < 0 || x > 11 || y - 1 < 0 || y + 1 > 19 ))
+        {
+            if (copy[x, y].name == copy[x, y + 1].name && copy[x, y].name == copy[x, y - 1].name)
+            {
+                CheckList.Add(new Vector2Int(x, y - 1));
+                CheckList.Add(new Vector2Int(x, y + 1));
+                flag = true;
+            }
+        }
+
+        if (!(x < 0 || x > 11 || y - 2 < 0 || y > 19 ))
+        {
+            if (copy[x, y].name == copy[x, y - 2].name && copy[x, y].name == copy[x, y - 1].name)
+            {
+                CheckList.Add(new Vector2Int(x, y - 2));
+                CheckList.Add(new Vector2Int(x, y - 1));
+                flag = true;
+            }
+        }
+
+        if(!flag)
+        {
+            return false;
+        }
+
+        foreach(var item in CheckList)
+        {
+            if (unit[item.x, item.y] != null)
+            {
+                Destroy(unit[item.x, item.y]);
+                unit[item.x, item.y] = null;
+            }
+        }
+
+        return true;
+    }
 
     public GameObject Spawn(GameObject g, Vector3 position)
     {
--- AniPang/Assets/Scripts/UnitMove.cs
+++ AniPang/Assets/Scripts/UnitMove.cs
@@ -7,6 +7,7 @@ public class UnitMove : MonoBehaviour {
     Vector3 origin;
 
     GameObject clickUnit;
+    GameObject nowUnit;
     bool firstCheck;
 
 	// Use this for initialization
@@ -36,17 +37,48 @@ public class UnitMove : MonoBehaviour {
                 clickUnit.transform.GetChild(0).localPosition = Vector3.zero;
                 return;
             }
-            if(Vector3.Distance(clickUnit.transform.position, g.transform.position) > 1.0f)
+            if(Vector3.Distance(clickUnit.transform.position, g.transform.position) > 1.2f)
             {
                 return;
             }
+            nowUnit = g;
             clickUnit.transform.GetChild(0).position = g.transform.position + new Vector3(0, 0, -0.1f) ; 
         }
 
         if(Input.GetMouseButtonUp(0))
         {
+            Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if(nowUnit == clickUnit)
+            {
+                origin = Vector3.zero;
+                clickUnit = null;
+                nowUnit = null;
+                return;
+            }
+            /*Vector3 tmp = nowUnit.transform.position;
+            nowUnit.transform.position = clickUnit.transform.position;
+            clickUnit.transform.position = tmp;
+
+            nowUnit.transform.GetChild(0).localPosition = Vector3.zero;
+            clickUnit.transform.GetChild(0).localPosition = Vector3.zero;*/
+
+            if(Camera.main.GetComponent<MainGame>().CheckPang(nowUnit, clickUnit))
+            {
+                Debug.Log("나 들옴");
+                Vector3 tmp = nowUnit.transform.position;
+                nowUnit.transform.position = clickUnit.transform.position;
+                clickUnit.transform.position = tmp;
+
+                nowUnit.transform.GetChild(0).localPosition = Vector3.zero;
+                clickUnit.transform.GetChild(0).localPosition = Vector3.zero;
+            }
+            else
+            {
+                clickUnit.transform.GetChild(0).localPosition = Vector3.zero;
+            }
+
             origin = Vector3.zero;
-            clickUnit.transform.GetChild(0).localPosition = Vector3.zero;
+            nowUnit = null;
             clickUnit = null;
         }
 	}
